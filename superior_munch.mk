e7685587c8fce00198eb876be57f9b9f233c0493
Atharva <atharva15.bgmi@gmail.com>
2023-08-14 22:07:32 +0530

    SuperiorExtended Bringup

--- superior_munch.mk
+++ superior_munch.mk
@@ -9,14 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-DEVICE_MAINTAINER := madmax7896
 
-PRODUCT_NAME := arrow_munch
+PRODUCT_NAME := superior_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -25,3 +24,16 @@ PRODUCT_MODEL := POCO F4
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V14.0.3.0.TLMMIXM:user/release-keys
+
+#inherit flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_MATLOG := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+USE_MOTO_CALCULATOR := true
+TARGET_SUPPORTS_BLUR := true
+USE_MOTO_CLOCK := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+BUILD_WITH_GAPPS := true
+USE_QUICKPIC := true
+USE_DUCKDUCKGO := false
---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/madmax7896/kernel_xiaomi_sm8250 kernel/xiaomi/sm8250
+git clone https://github.com/madmax7896/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
+git clone https://gitlab.com/madmax7896/android_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
+git clone https://gitlab.com/madmax7896/vendor_xiaomi_munch vendor/xiaomi/munch
+
