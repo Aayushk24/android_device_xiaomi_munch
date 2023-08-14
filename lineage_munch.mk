6dc7401d9a59e5e27282f0c440c9c4561c2940ad
Atharva <atharva15.bgmi@gmail.com>
2023-08-14 22:20:02 +0530

    RisingOs Bringup

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
 
-PRODUCT_NAME := superior_munch
+PRODUCT_NAME := lineage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -25,15 +25,15 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V14.0.3.0.TLMMIXM:user/release-keys
 
-#inherit flags
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_MATLOG := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
-USE_MOTO_CALCULATOR := true
-TARGET_SUPPORTS_BLUR := true
-USE_MOTO_CLOCK := true
-SYSTEM_OPTIMIZE_JAVA := true
-SYSTEMUI_OPTIMIZE_JAVA := true
-BUILD_WITH_GAPPS := true
-USE_QUICKPIC := true
-USE_DUCKDUCKGO := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+RISING_MAINTAINER := AayushK
+RISING_CHIPSET := SD870
+TARGET_ENABLE_BLUR := true
+TARGET_PREBUILT_GCAM := true
+WITH_GMS := true
+TARGET_BUILD_APERTURE_CAMERA := false
+
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,4 +2,4 @@ git clone https://github.com/madmax7896/kernel_xiaomi_sm8250 kernel/xiaomi/sm825
 git clone https://github.com/madmax7896/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
 git clone https://gitlab.com/madmax7896/android_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
 git clone https://gitlab.com/madmax7896/vendor_xiaomi_munch vendor/xiaomi/munch
-
+git clone https://github.com/madmax7896/device_xiaomi_munch-miuicamera device/xiaomi/munch-miuicamera
